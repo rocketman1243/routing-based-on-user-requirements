917c18393332a33915973abf189d76d4b4159ee6
Timon Bestebreur <timonbestebreur@gmail.com>
2023-06-22 15:11:44 +0200

    Finish building the graph!

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import json
 from types import SimpleNamespace
+import networkx as nx
+import copy
 
 nio_objects = []
+as_numbers = []
+edges = []
 
 for i in range(1, 21):
     filename = f"nio_files/nio_{i}.json"
@@ -10,6 +14,18 @@ for i in range(1, 21):
     nio_object = json.loads(nio_content, object_hook=lambda nio_content: SimpleNamespace(**nio_content))
     nio_objects.append(nio_object)
 
+    # Gather graph information
+    as_numbers.append(nio_object.as_number)
+    latencies = nio_object.latency
+
+    for index, outgoing_edge in enumerate(nio_object.connections):
+        edge_data = {
+            "latency": latencies[index]
+        }
+        edge_entry = [nio_object.as_number, outgoing_edge, edge_data]
+        edges.append(edge_entry)
+
+
 pro_objects = []
 
 for i in range(1, 11):
@@ -17,5 +33,9 @@ for i in range(1, 11):
     pro_file = open(filename)
     pro_content = pro_file.read()
     pro_object = json.loads(pro_content, object_hook=lambda pro_content: SimpleNamespace(**pro_content))
-    print(pro_object)
     pro_objects.append(pro_object)
+
+G = nx.Graph()
+G.add_nodes_from(as_numbers)
+G.add_edges_from(edges)
+
--- todo.md
+++ todo.md
@@ -1,15 +1,8 @@
 
 # The graph
-- Generate/create 10 nio json files
-- Create a python nio object
-- Read json file and create nio object for each file
 - create networkx graph based on nio objects
 
-
 # The filtersets
-- Generate/create 10 pro json files
-- Create a python pro object
-- Read json file and create pro object for each file
 - Create filtersets based on the nio objects
 - Create a mock loop in which the best effort part of the filterset is continuously updated with the next subset OR shorter list
 
