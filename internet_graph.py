0ead77b4c7714c4d9328177127af4333b3099d43
Timon Bestebreur <timonbestebreur@gmail.com>
2023-07-04 11:35:53 +0200

    Complete move

--- internet_graph.py
+++ internet_graph.py
@@ -3,7 +3,6 @@ import networkx as nx
 import json
 import matplotlib.pyplot as plt
 from types import SimpleNamespace
-import random, math
 
 nodes = []
 edges = []
@@ -35,29 +34,3 @@ G.add_edges_from(edges)
 print(len(G.nodes))
 print(len(G.edges))
 
-
-def generate_distribution(max_number_of_features: int, nr_of_ases: int) -> list[int]:
-    items_per_step = math.ceil(nr_of_ases / max_number_of_features)
-
-    distribution = []
-    counter = 0
-    current_number_of_elements = items_per_step
-    for i in range(nr_of_ases):
-        distribution.append(current_number_of_elements)
-        counter += 1
-
-        if counter >= items_per_step:
-            counter = 0
-            current_number_of_elements -= 1
-
-
-
-
-
-
-def generate_features(max_number_of_features: int, as_numbers: list[int]) -> list[int]:
-    features = range(1, 31)
-    mapping = []
-
-    for index, ass in enumerate(as_numbers):
-        sample = random.sample(features, )
