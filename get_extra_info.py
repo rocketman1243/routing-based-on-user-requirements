e1bda1e6094ba559cfc73dc0abd076ad843cbeb3
Timon Bestebreur <timonbestebreur@gmail.com>
2023-07-11 12:53:24 +0200

    Rewrite BING script to actual BING method #B(r)ING_IT

--- get_extra_info.py
+++ get_extra_info.py
@@ -4,30 +4,25 @@ from api_key import get_bing_apy_key # This key is in a local file. To reproduce
 import geocoder
 
 test_as = 198711
-url = "https://api.bgpview.io/asn/" # + AS number as int
 
-response = json.loads(requests.get(url + str(test_as)).text)
+def get_country_lat_lon_for_as(as_number):
+    url = "https://api.bgpview.io/asn/" # + AS number as int
 
-country = response["data"]["country_code"]
-address = response["data"]["owner_address"]
+    response = json.loads(requests.get(url + str(as_number)).text)
 
-address_as_string = ""
+    country = response["data"]["country_code"]
 
-for i, part in enumerate(address):
-    address_as_string += part 
-    if i < len(address) - 1:
-        address_as_string += ", "
+    address = response["data"]["owner_address"]
+    address_as_string = ""
 
-g = geocoder.bing(address_as_string, key=get_bing_apy_key())
-results = g.json
-lat = results['lat']
-lon = results['lon']
+    for i, part in enumerate(address):
+        address_as_string += part 
+        if i < len(address) - 1:
+            address_as_string += ", "
 
+    g = geocoder.bing(address_as_string, key=get_bing_apy_key())
+    results = g.json
+    lat = results['lat']
+    lon = results['lng']
 
-print(country)
-print(address)
-print(address_as_string)
-print(f"lat: {lat}, lon: {lon}")
-
-print("bing goodies along with lat/lon:")
-print(results)
+    return (country, lat, lon)
