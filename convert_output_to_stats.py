c01e20805563095b411cbc2631197e3f3f896980
Timon Bestebreur <timonbestebreur@gmail.com>
2023-10-18 16:58:50 +0200

    Update conversion script to handle Cost of control data

--- convert_output_to_stats.py
+++ convert_output_to_stats.py
@@ -5,16 +5,12 @@ from statistics import mean, median
 def spit_stats(input: list, stat: str):
     min_s = min(input)
     median_s = median(input)
-    average_s = round(mean(input))
+    average_s = round(mean(input), 1)
     max_s = max(input)
 
     print("stats for : ", stat, ": ", min_s, median_s, average_s, max_s)
 
 
-# 0,2,2,success,4.63,2.24,4.68,4.89,16.44,204441;48362;2134;7015;55175-112|204441;8437;3320;6461;13977;55175-121
-
-# new:
-# 3,2,2,success,5.45,1.41,3.28,4.85,14.99,395994;209;1239;204995-109|395994;33660;3356;204995-112#395994;33660;3356;204995-112
 
 
 # min, median, average, max of:
@@ -23,7 +19,7 @@ def spit_stats(input: list, stat: str):
 # - estimated total latency
 # - number of paths
 
-csv_path = "full_scale_setup/initial_results_experiment/results/output.csv"
+csv_path = "full_scale_setup/proof_of_concept_experiment/results/output.csv"
 
 number_of_paths = []
 hopcounts = []
@@ -48,37 +44,54 @@ with open(csv_path, "r") as file:
         number_of_paths.append(int(items[1]))
 
 
-        paths_and_shortest_path = items[len(items) - 1].split("#")
-        paths_and_latency = paths_and_shortest_path[0].split("|")
+        macro_path_separator = items[len(items) - 1].split("#")
+        paths_and_latency = macro_path_separator[0].split("|")
+
+        local_hopcounts = []
+        local_latencies = []
         
         for path_and_latency in paths_and_latency:
             path = path_and_latency.split("-")[0].split(";")
-            latency = path_and_latency.split("-")[1]
+            path_hopcount = len(path)
+            path_latency = int(path_and_latency.split("-")[1])
 
-            hopcounts.append(len(path))
-            latencies.append(int(latency))
+            local_hopcounts.append(path_hopcount)
+            local_latencies.append(path_latency)
+
+
+        hopcounts.extend(local_hopcounts)
+        latencies.extend(local_latencies)
 
         # Cost of control experiment
-        shortest_path_no_requirements = paths_and_shortest_path[1]
-        sp_path = shortest_path_no_requirements.split("-")[0].split(";")
-        sp_latency = int(shortest_path_no_requirements.split("-")[1])
-        sp_nr_hops = len(sp_path)
+        shortest_path_no_requirements = macro_path_separator[1]
+        shortest_path_no_requirement_path = shortest_path_no_requirements.split("-")[0].split(";")
+        shortest_path_no_requirement_nr_hops = len(shortest_path_no_requirement_path)
 
-        total_runtimes.append(items[8])
-        min_nr_hops_with_requirements = min(hopcounts)
-        min_latency_with_requirements = min(latencies)
-        extra_hops_due_to_requirements.append(min_nr_hops_with_requirements - sp_nr_hops)
-        extra_latency_due_to_requirements.append(min_latency_with_requirements - sp_latency)
+        fastest_path_no_requirements = macro_path_separator[2]
+        fastest_path_no_requirements_latency = int(fastest_path_no_requirements.split("-")[1])
 
+        total_runtimes.append(float(items[8]))
+        
+        min_nr_hops_with_requirements = min(local_hopcounts)
+        min_latency_with_requirements = min(local_latencies)
 
-print("fails: ", fails)
-print("nr_of_paths: ", number_of_paths)
-print("hopcounts: ", hopcounts)
-print("latencies:", latencies)
-print("total runtimes: ", total_runtimes)
+        extra_hops_due_to_requirements.append(min_nr_hops_with_requirements - shortest_path_no_requirement_nr_hops)
+        extra_latency_due_to_requirements.append(min_latency_with_requirements - fastest_path_no_requirements_latency)
 
 
-print("#fails: ", fails)
+# 1,2,2,success,5.87,1.79,1.38,5.98,15.02,26405;3356;8220;49065-108|26405;13354;49544;49079;49065-106#26405;3356;8220;49065-108#26405;13354;49544;49079;49065-106
+
+# print("fails: ", fails)
+# print("nr_of_paths: ", number_of_paths)
+# print("hopcounts: ", hopcounts)
+# print("latencies:", latencies)
+# print("total runtimes: ", total_runtimes)
+
+
+print("#fails: ", fails, "/", len(total_runtimes))
 spit_stats(number_of_paths, "number_of_paths")
 spit_stats(hopcounts, "hopcounts")
 spit_stats(latencies, "latencies")
+spit_stats(total_runtimes, "COC total runtimes")
+spit_stats(extra_hops_due_to_requirements, "COC extra hops")
+spit_stats(extra_latency_due_to_requirements, "COC extra latency")
